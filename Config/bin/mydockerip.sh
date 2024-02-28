79894fe2ff1f31a85d0a14d08e22af29b976be60
deeb <147339907+tryto-deeb@users.noreply.github.com>
2024-02-28 01:55:47 +0100

    mydockerip.sh

--- Config/bin/mydockerip.sh
+++ Config/bin/mydockerip.sh
@@ -4,9 +4,9 @@ IFACE=$(/usr/sbin/ifconfig | grep docker0 | awk '{print $1}' | tr -d ':')
 
 if [ "$IFACE" = "docker0" ]; then
 	echo "%{F#CCCC00} %{F#CCCC00}$(/usr/sbin/ifconfig docker0 | grep "inet " | awk '{print $2}')%{u-}"
-	echo "$(/usr/sbin/ifconfig docker0 | grep "inet " | awk '{print $2}')" > /home/user/.config/bin/mydocker_ip
+	echo "$(/usr/sbin/ifconfig docker0 | grep "inet " | awk '{print $2}')" > ~/.config/bin/mydocker_ip
 else
 	echo "%{F#CCCC00} %{u-}%{F#666633}Disconnected"
-	echo "" > /home/user/.config/bin/mydocker_ip
+	echo "" > ~/.config/bin/mydocker_ip
 fi
 
